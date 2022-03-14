e34d5171c1bd42ea8b615215826ec9cd4d788ea9
marcocarneiro <marco.carneiro7@gmail.com>
2022-03-13 22:41:26 -0300

    Codificação para bloco de slide show

--- mc-blocks.php
+++ mc-blocks.php
@@ -85,11 +85,62 @@ function video_background()
 }
 add_action( 'carbon_fields_register_fields', 'video_background' );
 
+/**
+ * Bloco para Sliders - carousel
+ * Bootstrap 5
+ */
+function sliders_container()
+{
+	Block::make( 'complex', 'slider_show', __( 'Slider Show' ) )
+	->set_inner_blocks( true )
+	->add_fields( array(
+		Field::make( 'text', 'title', __( 'Slide Title' ) ),
+	) )
+	->set_description( __( 'Container para slides. Adicione slides dentro desse bloco.' ) )
+	->set_category( 'custom-category', __( 'MC Blocks' ), 'smiley' )		
+	->set_icon( 'interactive' )
+	->set_render_callback( function ( $block ) {
+
+		ob_start();
+		?>
+			<!-- tags -->
+			<?php var_dump($block['slider_show']); ?>
+
+		<?php 
+		return ob_get_flush();
+	} );
+}
+add_action( 'carbon_fields_register_fields', 'sliders_container' );
+
+
+function slide()
+{
+	Block::make( 'Slide de imagens' )
+		->add_fields( array(
+			Field::make( 'text', 'legenda', __( 'Legenda do Slide' ) ),
+			Field::make( 'text', 'url', __( 'Link do slide' ) ),
+			Field::make( 'image', 'imagem', __( 'Imagem do slide' ) ),
+		) )
+	->set_description( __( 'Slides em carousel. Inserir' ) )
+	->set_category( 'custom-category', __( 'MC Blocks' ), 'smiley' )		
+	->set_icon( 'interactive' )
+	->set_render_callback( function ( $block ) {
+
+		ob_start();
+		?>
+			<!-- tags -->
+
+		<?php 
+		return ob_get_flush();
+	} );
+}
+add_action( 'carbon_fields_register_fields', 'slide' );
+
 
 
 /**
  * Bloco para simulação de financiamento
- * Bootstrap 4
+ * Bootstrap 5
  */
 function simulacao_financiamento()
 {
